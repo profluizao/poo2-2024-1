1e83b9ec38fe0b7d46ee1fee1f02cb208aafc259
Prof. Luiz Augusto Rodrigues <prof.luiz.a.rodrigues.2018@gmail.com>
2024-06-07 16:41:34 -0400

    Update ClasseController.java

--- Codigo/Spring Boot/Projeto Atacado Spring Boot/atacado/src/main/java/br/com/uniderp/poo2/atacado/Controllers/ClasseController.java
+++ Codigo/Spring Boot/Projeto Atacado Spring Boot/atacado/src/main/java/br/com/uniderp/poo2/atacado/Controllers/ClasseController.java
@@ -8,10 +8,13 @@ import br.com.uniderp.poo2.atacado.Repositories.IClasseRepository;
 
 import java.util.List;
 
+import org.springframework.http.MediaType;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
@@ -33,9 +36,27 @@ public class ClasseController {
         return instancia; 
     }
 
-    @PostMapping()
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
     public Classe Incluir(@RequestBody Classe instancia){
         Classe nova = this.repo.save(instancia);
         return nova;
+    }
+
+    @PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    public Classe Alterar(@RequestBody Classe instancia){
+        Classe alterada = this.repo.save(instancia);
+        return alterada;
+    }
+
+    @DeleteMapping("/{id}")
+    public Classe ExcluirPorId(@PathVariable Long id){
+        Classe paraExcluir = this.repo.findById(id).get();
+        this.repo.delete(paraExcluir);
+        return paraExcluir;
+    }
+
+    @DeleteMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    public Classe ExcluirPorInstancia(@RequestBody Classe instancia){
+        return this.ExcluirPorId(instancia.getCodigo());
     }
 }
